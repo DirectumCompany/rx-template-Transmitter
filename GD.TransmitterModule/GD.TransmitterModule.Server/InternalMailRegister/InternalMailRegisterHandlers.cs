7942b4af213ef8d77101bf7f4507f124c05e826f
Khafizov_IlI <Khafizov_IlI@DIRECTUM.ru>
2023-06-15 15:57:24 +0400

    1. Внес правки по результатам тестов

--- GD.TransmitterModule/GD.TransmitterModule.Server/InternalMailRegister/InternalMailRegisterHandlers.cs
+++ GD.TransmitterModule/GD.TransmitterModule.Server/InternalMailRegister/InternalMailRegisterHandlers.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -7,4 +7,13 @@ using GD.TransmitterModule.InternalMailRegister;
 
 namespace GD.TransmitterModule
 {
+  partial class InternalMailRegisterServerHandlers
+  {
+
+    public override void Created(Sungero.Domain.CreatedEventArgs e)
+    {
+      _obj.IsRequestTransfer = false;
+    }
+  }
+
 }
