25c98876f517f42f5cd44325d6942601b6cb0d9f
Ilya Zaytsev <Zaytsev_IS@directum.ru>
2024-02-01 14:48:25 +0400

    Добавил синхронизацию вложений к входящему документу

--- GD.TransmitterModule/GD.TransmitterModule.ClientBase/IncomingDocumentProcessingReworkAssignment/IncomingDocumentProcessingReworkAssignmentActions.cs
+++ GD.TransmitterModule/GD.TransmitterModule.ClientBase/IncomingDocumentProcessingReworkAssignment/IncomingDocumentProcessingReworkAssignmentActions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
@@ -21,7 +21,43 @@ namespace GD.TransmitterModule.Client
 
     public virtual void Corrected(Sungero.Workflow.Client.ExecuteResultActionArgs e)
     {
+      var documents = _obj.AddendaGroup.ElectronicDocuments.Where(x => Sungero.Content.ElectronicDocuments.Is(x)).ToList();
+      var documentProcessingTask = IncomingDocumentProcessingTasks.As(_obj.MainTask).AddendaGroup.ElectronicDocuments.ToList();
+      var addendumDocuments = new List<Sungero.Content.IElectronicDocument>();
+      var addendums = new List<Sungero.Docflow.IAddendum>();
+
+      var elements = documents.Except(documentProcessingTask);
+      addendumDocuments.AddRange(elements);
       
+      foreach (var document in addendumDocuments)
+      {
+        if (Sungero.Content.ElectronicDocuments.Is(document))
+        {
+          var createdAddendum = Sungero.Docflow.Addendums.Create();
+          
+          if (!createdAddendum.HasVersions)
+            createdAddendum.CreateVersion();
+          
+          using (var docBodyStream = new System.IO.MemoryStream())
+          {
+            using (var sourceStream = document.LastVersion.Body.Read())
+              sourceStream.CopyTo(docBodyStream);
+
+            createdAddendum.LastVersion.Body.Write(docBodyStream);
+          }
+          
+          createdAddendum.LastVersion.AssociatedApplication = document.AssociatedApplication;
+          createdAddendum.LeadingDocument = Sungero.Docflow.OfficialDocuments.As(document);
+          createdAddendum.Save();
+          addendums.Add(createdAddendum);
+        }
+      }
+      
+      foreach (var addendum in addendums)
+      {
+        var addendumsIncomingDocumentProcessingTasks = IncomingDocumentProcessingTasks.As(_obj.MainTask).Addendums.AddNew();
+        addendumsIncomingDocumentProcessingTasks.Reason = addendum;
+      }
     }
 
     public virtual bool CanCorrected(Sungero.Workflow.Client.CanExecuteResultActionArgs e)
