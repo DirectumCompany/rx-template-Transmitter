6134775fc030a192592ef1e144ae149494cb038d
Ilya Zaytsev <Zaytsev_IS@directum.ru>
2024-07-17 14:33:27 +0400

    Доработки

--- GD.TransmitterModule/GD.TransmitterModule.ClientBase/IncomingDocumentProcessingReworkAssignment/IncomingDocumentProcessingReworkAssignmentActions.cs
+++ GD.TransmitterModule/GD.TransmitterModule.ClientBase/IncomingDocumentProcessingReworkAssignment/IncomingDocumentProcessingReworkAssignmentActions.cs
@@ -21,38 +21,13 @@ namespace GD.TransmitterModule.Client
 
     public virtual void Corrected(Sungero.Workflow.Client.ExecuteResultActionArgs e)
     {
-      var documents = _obj.AddendaGroup.ElectronicDocuments.Where(x => Sungero.Content.ElectronicDocuments.Is(x)).ToList();
+      var documents = _obj.AddendaGroup.ElectronicDocuments.Where(doc => Sungero.Content.ElectronicDocuments.Is(doc)).ToList();
       var documentProcessingTask = IncomingDocumentProcessingTasks.As(_obj.MainTask).AddendaGroup.ElectronicDocuments.ToList();
-      var addendums = new List<Sungero.Docflow.IAddendum>();
-      
+
       foreach (var document in documents.Except(documentProcessingTask))
       {
-        if (Sungero.Content.ElectronicDocuments.Is(document))
-        {
-          var createdAddendum = Sungero.Docflow.Addendums.Create();
-          
-          if (!createdAddendum.HasVersions)
-            createdAddendum.CreateVersion();
-          
-          using (var docBodyStream = new System.IO.MemoryStream())
-          {
-            using (var sourceStream = document.LastVersion.Body.Read())
-              sourceStream.CopyTo(docBodyStream);
-
-            createdAddendum.LastVersion.Body.Write(docBodyStream);
-          }
-          
-          createdAddendum.LastVersion.AssociatedApplication = document.AssociatedApplication;
-          createdAddendum.LeadingDocument = Sungero.Docflow.OfficialDocuments.As(document);
-          createdAddendum.Save();
-          addendums.Add(createdAddendum);
-        }
-      }
-      
-      foreach (var addendum in addendums)
-      {
         var addendumsIncomingDocumentProcessingTasks = IncomingDocumentProcessingTasks.As(_obj.MainTask).Addendums.AddNew();
-        addendumsIncomingDocumentProcessingTasks.Reason = addendum;
+        addendumsIncomingDocumentProcessingTasks.Reason = document;
       }
     }
 
